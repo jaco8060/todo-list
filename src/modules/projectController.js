6822576d3e4a893c90c3863e216f8dafe85a23de
Jeremiah <jeremiahjacobson10@gmail.com>
2024-02-05 17:42:20 -0500

    added remove project task function

--- src/modules/projectController.js
+++ src/modules/projectController.js
@@ -4,6 +4,7 @@ class Project {
   constructor(projectName) {
     this._projectName = projectName;
     this._todoList = [];
+    localStorage.setItem("projectStorage", JSON.stringify(todoItem));
   }
 
   get projectName() {
@@ -17,15 +18,17 @@ class Project {
     // Create todo list object
     const todoItem = new Todo(title, details, date);
     this._todoList.push(todoItem);
+    // save to storage
+
     allTodos.appendTodo(todoItem);
   }
 
   removeProjectTask = (todoToRemove) => {
     const index = this._todoList.findIndex((todo) => todo === todoToRemove);
     if (index !== -1) {
-      this._todoList.splice(index, 1); // Remove the todo if found
+      this._todoList.splice(index, 1); // Remove the todo if found from the project list
     }
-    allTodos.removeTodo(todoToRemove);
+    allTodos.removeTodo(todoToRemove); //also remove from the all todos list
   };
 }
 
