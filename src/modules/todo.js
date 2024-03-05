a0b9bb3818a0a72a6a41a2aca518720d7ba2e181
Jeremiah <jeremiahjacobson10@gmail.com>
2024-03-04 23:03:22 -0500

    added toJSON method for Todo object and also ensured that date is an object

--- src/modules/todo.js
+++ src/modules/todo.js
@@ -1,7 +1,8 @@
 import { isBefore, startOfToday } from "date-fns";
+
 class Todo {
   constructor(title, details, date) {
-    this._title = title; // Using underscore-prefixed private property
+    this._title = title;
     this._details = details;
     this._date = date instanceof Date ? date : new Date(date); // Ensure _date is a Date object
     this._starred = false;
@@ -21,10 +22,18 @@ class Todo {
     return this._date;
   }
 
+  get isOverdue() {
+    return this._isOverdue;
+  }
+
+  get starred() {
+    return this._starred;
+  }
+
   // Setters
   set title(newTitle) {
-    // Optionally, add validation or other logic here
     this._title = newTitle;
+    this.updateOverdueStatus(); // Update overdue status in case the change affects it
   }
 
   set details(newDetails) {
@@ -32,14 +41,12 @@ class Todo {
   }
 
   set date(newDate) {
-    this._date = newDate;
+    this._date = newDate instanceof Date ? newDate : new Date(newDate);
+    this.updateOverdueStatus(); // Always update overdue status when date changes
   }
 
   updateOverdueStatus() {
     this._isOverdue = isBefore(this._date, startOfToday());
-  }
-  get isOverdue() {
-    return this._isOverdue;
   }
 
   // Method to mark the todo as starred
@@ -52,8 +59,15 @@ class Todo {
     this._starred = false;
   }
 
-  get starred() {
-    return this._starred;
+  // Method to prepare Todo for JSON serialization
+  toJSON() {
+    return {
+      title: this._title,
+      details: this._details,
+      date: this._date.toISOString(), // Convert Date to ISO string for serialization
+      starred: this._starred,
+      isOverdue: this._isOverdue, // Optionally include if you want to persist this status
+    };
   }
 }
 
